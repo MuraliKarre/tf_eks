97ffc36684f1133f520ab2dbc12b31964d215115
Murali <muralidevops18@gmail.com>
2022-11-13 14:39:06 +0530

    Added subnets

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
  variable "subnet_id_1" {
   type = string
-  default = "subnet-your_first_subnet_id"
+  default = "subnet-01e72663b70e3a56a"
  }
  
  variable "subnet_id_2" {
   type = string
-  default = "subnet-your_second_subnet_id"
+  default = "subnet-07d0b5811eab57856"
  }
